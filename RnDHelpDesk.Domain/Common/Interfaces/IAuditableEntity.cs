ecb30a354b086d9153ad4d477ca8ca92f3f8d8ce
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-11-03 13:35:28 +0700

    feat: IEntity and IAuditableEntity

---
+++ RnDHelpDesk.Domain/Common/Interfaces/IAuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnDHelpDesk.Domain.Common.Interfaces
+{
+    public interface IAuditableEntity : IEntity
+    {
+        int? CreatedBy { get; set; }
+        DateTime? CreatedDate { get; set; }
+        int? UpdatedBy { get; set; }
+        DateTime? UpdatedDate { get; set; }
+    }
+}
