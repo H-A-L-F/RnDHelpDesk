c0d167ce6f67a2ed0dac5c8c8e4e9f1a4d8c91cc
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-11-03 14:40:25 +0700

    feat: create User entity

---
+++ RnDHelpDesk.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using RnDHelpDesk.Domain.Common;
+using RnDHelpDesk.Domain.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnDHelpDesk.Domain.Entities
+{
+    public class User : BaseAuditableEntity
+    {
+        public string Username { get; set; }
+        public Role Role { get; set; }
+    }
+}
