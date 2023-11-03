7242297633c418bade287350e581608e6e5af041
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-11-03 15:57:59 +0700

    feat: Created repositories

---
+++ RnDHelpDesk.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using RnDHelpDesk.Domain.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnDHelpDesk.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : class, IEntity
+    {
+        IQueryable<T> Entities { get; }
+
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsnyc(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ RnDHelpDesk.Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using RnDHelpDesk.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnDHelpDesk.Application.Interfaces.Repositories
+{
+    internal interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> Repository<T>() where T : BaseAuditableEntity;
+        Task<int> Save(CancellationToken cancellationToken);
+        Task<int> SaveAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys);
+        Task Rollback();
+    }
+}
