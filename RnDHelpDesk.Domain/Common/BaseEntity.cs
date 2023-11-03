e41aa9d847b408af898a0e729616cf22b80900f4
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-11-03 13:38:33 +0700

    feat: added BaseEntity and BaseAuditableEntity

---
+++ RnDHelpDesk.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using RnDHelpDesk.Domain.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnDHelpDesk.Domain.Common
+{
+    public abstract class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
