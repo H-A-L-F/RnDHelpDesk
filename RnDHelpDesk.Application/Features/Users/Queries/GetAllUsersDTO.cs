b8fb8898c53e85e4aa3768c84d422b677bde6f5b
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-12-07 11:59:23 +0700

    feat: added GetAllUsers

---
+++ RnDHelpDesk.Application/Features/Users/Queries/GetAllUsersDTO.cs
@@ -0,0 +1,19 @@
+﻿using RnDHelpDesk.Application.Common.Mappings;
+using RnDHelpDesk.Domain.Entities;
+using RnDHelpDesk.Domain.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnDHelpDesk.Application.Features.Users.Queries
+{
+    public class GetAllUsersDTO : IMapFrom<User>
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
---
+++ RnDHelpDesk.Application/Features/Users/Queries/GetAllUsersQuery.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RnDHelpDesk.Application.Interfaces.Repositories;
+using RnDHelpDesk.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnDHelpDesk.Application.Features.Users.Queries
+{
+    public record GetAllUsersQuery : IRequest<List<GetAllUsersDTO>>;
+    internal class GetAllUsersQueryHandler : IRequest<List<GetAllUsersDTO>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetAllUsersQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetAllUsersDTO>> Handle(GetAllUsersQuery query, CancellationToken cancellationToken)
+        {
+            return await _unitOfWork.Repository<User>().Entities.ProjectTo<GetAllUsersDTO>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+        }
+    }
+}
