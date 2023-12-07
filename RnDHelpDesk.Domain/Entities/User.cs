f0e6704463fe57e347f8bfc563a7a0f0513ae07c
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-12-07 11:43:06 +0700

    added password and constructor for User

--- RnDHelpDesk.Domain/Entities/User.cs
+++ RnDHelpDesk.Domain/Entities/User.cs
@@ -11,6 +11,14 @@ namespace RnDHelpDesk.Domain.Entities
     public class User : BaseAuditableEntity
     {
         public string Username { get; set; }
+        public string Password { get; set; }
         public Role Role { get; set; }
+
+        public User(string username, string password, Role role)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Role = role;
+        }
     }
 }
