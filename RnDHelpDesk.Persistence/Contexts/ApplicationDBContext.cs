32f877ede30fb4de0c1856e2af71637d7936060e
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-12-07 12:39:28 +0700

    feat: added DBContext

---
+++ RnDHelpDesk.Persistence/Contexts/ApplicationDBContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RnDHelpDesk.Domain.Entities;
+using System.Reflection;
+
+namespace RnDHelpDesk.Persistence.Contexts
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base() { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public override int SaveChanges()
+        {
+            return SaveChangesAsync().GetAwaiter().GetResult();
+        }
+    }
+}
